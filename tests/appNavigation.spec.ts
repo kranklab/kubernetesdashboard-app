7a00e9fc68ed3624794283e5bedbb57133ae722f
sdonnell <samdonnelly123@gmail.com>
2026-03-03 17:43:41 +0100

    Updated appNavigation test to use role-based assertion for 'Cluster Overview' heading visibility.

--- tests/appNavigation.spec.ts
+++ tests/appNavigation.spec.ts
@@ -4,7 +4,7 @@ import { ROUTES } from '../src/constants';
 test.describe('navigating app', () => {
   test('page Home should render successfully', async ({ gotoPage, page }) => {
     await gotoPage(`/${ROUTES.Home}`);
-    await expect(page.getByText('Cluster Overview')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Cluster Overview' })).toBeVisible();
   });
 
 });
